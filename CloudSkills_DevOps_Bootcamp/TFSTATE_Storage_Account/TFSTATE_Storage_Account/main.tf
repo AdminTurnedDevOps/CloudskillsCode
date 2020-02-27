d3a550b9f3211814464ebd89607746d674a6a7ef
Michael Levan <mlevan1992@gmail.com>
2020-02-27 12:37:40 -0500

    PowerShell code added to pull Kubeconfig

---
+++ CloudSkills_DevOps_Bootcamp/TFSTATE_Storage_Account/TFSTATE_Storage_Account/main.tf
@@ -0,0 +1,7 @@
+provider azurerm {
+    version = "2.0.0"
+}
+
+resource "azurerm_storage_account" "cloudSkillsStorageAccount" {
+    
+}
--- CloudSkills_DevOps_Bootcamp/TFSTATE_Storage_Account/terraform.tfvars
+++
@@ -1,3 +0,0 @@
-provider azurerm {
-    version = "2.0.0"
-}
