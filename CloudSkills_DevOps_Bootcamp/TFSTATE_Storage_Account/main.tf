b4b7ae7065d28fc1e7a1c7f518bd68814b7c9f5e
Michael Levan <mlevan1992@gmail.com>
2020-02-29 09:07:39 -0500

    Storage account for bootcamp

--- CloudSkills_DevOps_Bootcamp/TFSTATE_Storage_Account/main.tf
+++ CloudSkills_DevOps_Bootcamp/TFSTATE_Storage_Account/main.tf
@@ -1,19 +1,18 @@
 provider azurerm {
-    version = "2.0.0"
-    features {}
+  version = "2.0.0"
+  features {}
 }
 
 resource "azurerm_storage_account" "CloudSkillsStorageAccount" {
-    name = "tfteststorage"
-    resource_group_name = var.resourceGroup
-    location = var.location
-    account_tier = "Standard"
-    account_replication_type = "LRS"
+  name                     = "tfteststorage"
+  resource_group_name      = var.resourceGroup
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 }
 
 resource "azurerm_storage_container" "CloudSkillsContainer" {
-    name = "TFSTATE"
-    resource_group_name = var.resourceGroup
-    storage_account_name = azurerm_storage_account.CloudSkillsStorageAccount.name
-    container_access_type = "blob"
+  name                  = "tfstate"
+  storage_account_name  = azurerm_storage_account.CloudSkillsStorageAccount.name
+  container_access_type = "blob"
 }
