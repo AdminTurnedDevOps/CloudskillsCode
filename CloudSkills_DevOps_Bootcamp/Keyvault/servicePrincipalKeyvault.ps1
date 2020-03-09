d18389bada2b8063cd6b82f6d4681119bf280289
Michael Levan <mlevan1992@gmail.com>
2020-03-09 07:02:35 -0400

    Create keyvault with subscription ID access to the keyvault

--- CloudSkills_DevOps_Bootcamp/Keyvault/servicePrincipalKeyvault.ps1
+++ CloudSkills_DevOps_Bootcamp/Keyvault/servicePrincipalKeyvault.ps1
@@ -3,10 +3,35 @@ function servicePrinicpalKeyvault {
     param(
         [string]$name = "sp-keyvault-cloudskills",
         [string]$resourceGroup = "Dev10",
-        [string]$location = "eastus2"
+        [string]$location = "eastus2",
+        [string]$subscriptionName = "Mike-Pay-As-You-Go"
     )
 
-    $createKeyVault = az keyvault create --location $location --name $name --resource-group $resourceGroup
-    $createKeyVault
-    $createKeyVault.Id
+    try {
+        $createKeyVault = az keyvault create --location $location --name $name --resource-group $resourceGroup
+        $createKeyVault
+    }
+    catch {
+        Write-Warning 'An error has occurred. Please ensure the vault created properly...'
+        $PSCmdlet.ThrowTerminatingError($_)
+    }
+
+    try {
+        $subscriptionID = az account show --subscription $subscriptionName | ConvertFrom-Json | select -ExpandProperty id
+        az keyvault set-policy --name $name --object-id $subscriptionID --secret-permissions get list
+    }
+    catch {
+        Write-Warning "KeyVault permissions for $name were not set..."
+        $PSCmdlet.ThrowTerminatingError($_)
+    }
+
+    if ($PSCmdlet.ShouldProcess('name')) {
+        try {
+            $createKeyVault.Id
+        }
+        catch {
+            Write-Warning 'KeyVault vault does not exist...'
+            $PSCmdlet.ThrowTerminatingError($_)
+        }
+    }
 }
