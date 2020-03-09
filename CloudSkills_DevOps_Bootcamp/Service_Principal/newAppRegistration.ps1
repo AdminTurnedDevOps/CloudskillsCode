a958ee84d219e5550f462d7030fa3511ce7696f9
Michael Levan <mlevan1992@gmail.com>
2020-03-09 07:03:35 -0400

    Removed comments

--- CloudSkills_DevOps_Bootcamp/Service_Principal/newAppRegistration.ps1
+++ CloudSkills_DevOps_Bootcamp/Service_Principal/newAppRegistration.ps1
@@ -12,9 +12,6 @@ function newAppRegsitration {
     # Retrieve the Client ID for KeyVault
     $clientID = az ad app list --display-name $name --query "[].{SyncRoot:appId}" | ConvertFrom-Json | Select -ExpandProperty SyncRoot
     
-    #$clientSecret = az ad app credential reset --id $clientID.SyncRoot | ConvertFrom-Json | select -ExpandProperty password
-    #$clientSecret
-    
     az keyvault secret set --vault-name $keyvaultName --name "AKSClientID" --value $clientID.SyncRoot
     az keyvault secret set --vault-name $keyvaultName --name "AKSClientSecret" --value $appCreation.password
     }
