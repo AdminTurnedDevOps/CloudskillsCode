81ee0f2aeebc8c93818025779a6f220c89bdb767
Michael Levan <mlevan1992@gmail.com>
2020-03-09 08:02:24 -0400

    Added PowerShell to pull the Id for CloudSkills Azure DevOps project

--- CloudSkills_DevOps_Bootcamp/Keyvault/servicePrincipalKeyvault.ps1
+++ CloudSkills_DevOps_Bootcamp/Keyvault/servicePrincipalKeyvault.ps1
@@ -4,7 +4,7 @@ function servicePrinicpalKeyvault {
         [string]$name = "sp-keyvault-cloudskills",
         [string]$resourceGroup = "Dev10",
         [string]$location = "eastus2",
-        [string]$azureDevOpsClientID = "de132d37-4d6a-4b24-b97d-4b458d4e216d"
+        [string]$clientID = ""
     )
 
     try {
@@ -17,7 +17,9 @@ function servicePrinicpalKeyvault {
     }
 
     try {
-        az keyvault set-policy --name $name --object-id $azureDevOpsClientID --secret-permissions get list
+        #Connect-AzAccount -Subscription $azureSubscription
+        $ID = (Get-AzADServicePrincipal -ServicePrincipalName $clientID)
+        az keyvault set-policy --name $name --object-id $ID.Id --secret-permissions get list
     }
     catch {
         Write-Warning "KeyVault permissions for $name were not set..."
